06e96f1cf3819194c3ab11e2ea7523166f8591fe
YuigaWada <yuigawada@gmail.com>
2023-07-06 17:02:06 +0900

    [fix] fix bugs

--- preprocess/create_dataset_for_da.py
+++ preprocess/create_dataset_for_da.py
@@ -10,6 +10,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision
+import argparse
 from torch import Tensor
 from torch.utils.data import Dataset
 from tqdm import tqdm
@@ -19,15 +20,16 @@ from types import *
 class ShichimiDataset(Dataset):
     def __init__(self,
                  dataset_type: str,
+                 train_ratio: str,
                  path: str = "../data/"):
 
         print(f"====== {dataset_type} ======")
 
-        data = self._build(path,dataset_type)
+        data = self._build(path,dataset_type,train_ratio)
         self.data = data
         self.is_training = dataset_type == "train"
 
-    def _build(self,path_str: str,dataset_type: str):
+    def _build(self,path_str: str,dataset_type: str,train_ratio: float):
         with open(os.path.join(path_str, "stair_captions_v1.2_val.json"), 'r') as f:
             stair = json.load(f)
 
@@ -38,19 +40,21 @@ class ShichimiDataset(Dataset):
         for ann in annotations:
             imgid_to_captions.setdefault(ann["image_id"],[]).append(ann["caption"])
         
+        data = {}
         with connect_db() as db:
             cursor = db.cursor()
             cursor.execute("SELECT im.id, c.caption, r.mos, im.url  FROM results r INNER JOIN captions c ON r.caption_id = c.id INNER JOIN images im ON im.id = c.image_id")
-            data = cursor.fetchall()
-            data = [(imgid,cap,mos,imgid_to_captions[image_to_imgid[url]]) for (imgid,cap,mos,url) in data]
+            for (imgid,cap,mos,url) in cursor.fetchall():
+                data.setdefault(imgid,[]).append((imgid,cap,mos,imgid_to_captions[image_to_imgid[url]]))
             cursor.close()
-            
-        random.seed(42); random.shuffle(data)
-        data = self._split(dataset_type,data)
+        
+        imgids = list(data.keys())
+        random.seed(42); random.shuffle(imgids)
+        data = self._split(dataset_type,imgids,data,train_ratio)
         return data
 
-    def _split(self,dataset_type,data):
-        train_r, val_r, test_r = 0.5, 0.5, 0.0
+    def _split(self,dataset_type,imgids,data,train_ratio):
+        train_r, val_r, test_r = train_ratio, 1.0 - train_ratio, 0.
         if dataset_type == "train":
             left = len(data) * 0
             right = len(data) * train_r
@@ -62,7 +66,12 @@ class ShichimiDataset(Dataset):
             right = len(data) * (train_r + val_r + test_r)
         
         left, right = map(int,(left,right))
-        return data[left:right]
+        imgids = imgids[left:right]
+        splited = []
+        for d in [data[imgid] for imgid in imgids]:
+            splited.extend(d)
+        return splited 
+
 
     def __len__(self) -> int:
         """
@@ -80,9 +89,9 @@ class ShichimiDataset(Dataset):
         return img_id, cap, gt_caps, mos
 
 
-def main():
+def main(args):
     splits = ["train", "val", "test"]
-    datasets = [(s,ShichimiDataset(s)) for s in splits]
+    datasets = [(s,ShichimiDataset(s,args.train_r)) for s in splits]
     # data,lp,src,ref,pos,neg,pos.model,neg.model,bestmodel
 
 
@@ -125,4 +134,7 @@ def main():
                 f.write(f"{tpl['imgid']},\"{tpl['src']}\",\"{tpl['mt']}\",\"{tpl['ref']}\",{tpl['score']}\n")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_r', default=0.5,type=float)
+    args = parser.parse_args()
+    main(args)
